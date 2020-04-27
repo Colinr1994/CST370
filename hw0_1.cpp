b0fb694d7002549cda25a701a37b22379c7bbd58
Colinr1994 <Colinr1994@gmail.com>
2020-04-27 14:36:13 -0700

    added student id num

--- hw0_1.cpp
+++ hw0_1.cpp
@@ -5,6 +5,7 @@
  * Author: Colin Reed
  * ID: 004012715
  * Date: 04/27/2020
+ * HackerRank: https://www.hackerrank.com/contests/cst370-su20-hw0/challenges/370-hw0-1/submissions/code/1323171837
  */
 
 #include <iostream>
