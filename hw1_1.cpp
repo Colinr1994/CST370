cbf51bcac1044508f33f9560dd55db1014848499
Colinr1994 <Colinr1994@gmail.com>
2020-04-27 17:04:46 -0700

    init hw1

---
+++ hw1_1.cpp
@@ -0,0 +1,18 @@
+/* Title: hw0_1.cpp
+ * Abstract: This program reads the user's input data and
+ *           conducts sum and difference operations, depending
+ *           on the command key.
+ * Author: Colin Reed
+ * ID: 1234
+ * Date: 04/27/2020
+ * HackerRank: https://www.hackerrank.com/contests/cst370-su20-hw0/challenges/370-hw0-1/submissions/code/1323171837
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+   return 0;
+}
