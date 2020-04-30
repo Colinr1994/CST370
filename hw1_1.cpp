d476ed4f593dc3ec3a64df4fdab2221257ffb885
Colinr1994 <Colinr1994@gmail.com>
2020-04-30 14:32:50 -0700

    working on hw1

--- hw1_1.cpp
+++ hw1_1.cpp
@@ -1,18 +1,74 @@
-/* Title: hw0_1.cpp
- * Abstract: This program reads the user's input data and
- *           conducts sum and difference operations, depending
- *           on the command key.
+/* Title: hw1_1.cpp
+ * Abstract: This program accepts an input array and returns it sorted
+ * with repetitions removed.
+ *
  * Author: Colin Reed
  * ID: 1234
- * Date: 04/27/2020
+ * Date: 04/29/2020
  * HackerRank: https://www.hackerrank.com/contests/cst370-su20-hw0/challenges/370-hw0-1/submissions/code/1323171837
  */
 
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+	// Creating Variables and collecting arrays
+	int length;
+	int length2;
 
 
-   return 0;
+	cin >> length;
+
+	int arr[length];
+	int input;
+
+	for(int i=0; i<length;i++) {
+		cin >> input;
+		arr[i] = input;
+	};
+
+	cin >> length2;
+	int arr2[length2];
+	for(int i=0; i<length2;i++) {
+		cin >> input;
+		arr2[i] = input;
+	}
+	// Input Complete
+
+	int out[length];
+
+	int c=0;
+	for(int i=0; i<length; i++) {
+		for(int j=0; j<length2; j++) {
+			if(arr[i] == arr2[j]) {
+				out[i] =1;
+			}
+		}
+	}
+
+	int count =0;
+	for(int i=0; i<length; i++) {
+		if(out[i]==1) {
+			count++;
+		}
+	}
+
+	int end[count];
+
+	int turn =0;
+	for(int i=0;i<length; i++) {
+		if(out[i]==1) {
+			end[turn] = arr[i];
+			turn++;
+		}
+	}
+
+
+	for(int i=0; i<length; i++) {
+		cout << end[i] << endl;
+	}
+
+
+	return 0;
 }
---
+++ hw1_2.cpp
@@ -0,0 +1,20 @@
+/* Title: hw1_2.cpp
+ * Abstract: This program accepts an input array and returns it sorted
+ * with repetitions removed.
+ *
+ * Author: Colin Reed
+ * ID: 1234
+ * Date: 04/30/2020
+ * HackerRank: https://www.hackerrank.com/contests/cst370-su20-hw0/challenges/370-hw0-1/submissions/code/1323171837
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//
+
+
+	return 0;
+}
