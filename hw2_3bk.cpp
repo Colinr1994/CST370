4e95ff2c26d31bfc0c36e7274bcd361647659a17
Colinr1994 <Colinr1994@gmail.com>
2020-05-05 16:03:44 -0700

    2_3 working except for inputgit status

--- hw2_3bk.cpp
+++ hw2_3bk.cpp
@@ -12,31 +12,20 @@
 using namespace std;
 
 
+
 int main() {
   int vertices,edges,x,y;
-  string str;
 
   cin >> vertices;
   cin >> edges; // edges <= vertices
 
   // 2d bool array represents all possible edges
-  bool data[vertices][vertices];
-
-
-// WTF
-  for(x=0;x<vertices;x++) {
-    for(y=0;y<vertices;y++) {
-      data[x][y] = false;
-    }
-  }
+  bool data[vertices][vertices] = {0}; //Poor Assignment Practice?
 
   for (int i=0; i<edges;i++) {
-    // cin >> str;
-    // x = (int)str[0];
-    // x = (int)str[2];
     cin >> x;
     cin >> y;
-    data[x][y] = true;
+    data[x][y] = 1;
   }
 
   for(x=0;x<vertices;x++) {
