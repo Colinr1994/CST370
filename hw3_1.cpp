2155b9bb7bb3703e1d2ef11294dc4f84a3f79af3
Colinr1994 <Colinr1994@gmail.com>
2020-05-07 12:12:26 -0700

    progress

--- hw3_1.cpp
+++ hw3_1.cpp
@@ -4,28 +4,50 @@
  *
  * Author: Colin Reed
  * ID: 1234
- * Date: 05/4/2020
- * HackerRank: https://www.hackerrank.com/contests/cst370-su20-hw2/challenges/adjacency-list/submissions/code/1323379255
+ * Date: 05/07/2020
+ * HackerRank:
  */
 
 #include <iostream>
 using namespace std;
 
 bool palindrome(string in) {
-  if (in.empty()) {
+  if (in.empty() || in.length()==1)
     return 1;
-  } else {
-    if(in.back() == in.front()) {
-      palindrome("a");
-    } else { return 0;}
-  }
+  else
+    if (in.back() == in.front()) {
+      in.erase(0,1);
+      in.erase(in.size()-1);
+      return palindrome(in);
+    } else
+      return 0;
+}
+
+bool removeChar(char in) {
+  if(in == ' ') {
+      return true;
+  } else
+    return false;
+
 }
 
 
 int main() {
-  bool var = palindrome("a");
+  string input;
+  cin >> input;
 
-  cout << var;
+  //input = remove_if(input.begin(), input.end(), removeChar);
+
+  for (size_t i = 0; i < input.length(); i++) {
+    if (removeChar(input[i])) {
+      cout << input[i] << endl;
+      input.erase(i,1);
+    }
+  }
+
+  bool var = palindrome(input);
+
+  cout << var << endl;
 
   return 0;
 }
