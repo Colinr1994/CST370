68261cbc1fa8c05f738c540e64af20a5bcbfdf91
Colinr1994 <Colinr1994@gmail.com>
2020-05-06 20:54:11 -0700

    init hw31

---
+++ hw3_1.cpp
@@ -0,0 +1,20 @@
+/* Title: hw2_3.cpp
+ * Abstract: This program converts user input directed graph data into adjacent list format.
+ *
+ *
+ * Author: Colin Reed
+ * ID: 1234
+ * Date: 05/4/2020
+ * HackerRank: https://www.hackerrank.com/contests/cst370-su20-hw2/challenges/adjacency-list/submissions/code/1323379255
+ */
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+
+
+  return 0;
+}
