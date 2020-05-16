5fd786871a13df3264a18d545a1da116ee929511
Colinr1994 <Colinr1994@gmail.com>
2020-05-15 20:55:12 -0700

    4.1 init

---
+++ hw4_1.cpp
@@ -0,0 +1,60 @@
+/* Title: hw4_1.cpp
+ * Abstract:
+ *
+ * Reads inpit graph data from user. 
+ * Presents path for traveling salesman problem. 
+ * Maximum number of verticies <= 20
+ *
+ *
+ * Author: Colin Reed
+ * ID: 1234
+ * Date: 05/15/2020
+ * HackerRank: 
+ */
+
+
+/* Notes: 
+ *
+ * Input: 
+ *
+ * 	Int -> number of vetices in the graph 
+ *
+ * 	string -> names of verices (First input is starting point)
+ * 		(Always one word)
+ *
+ * 	int -> number of edges in the graph
+ *
+ * 	string? -> edge information. name1(Source) name2(dest) cost
+ *
+ * Output: 
+ *
+ * "Path:" 
+ *
+ *
+ *
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+/* perm class holds a permutation of the available vertices
+ * 
+ */
+class perm {
+	public: 
+		//int index? 
+		vector<string> path;
+		int cost;
+		bool passed;
+
+}
+
+int main() {
+	// testing input 
+	int input1 = 4;
+	vec 
+
+	return 1;
+}
