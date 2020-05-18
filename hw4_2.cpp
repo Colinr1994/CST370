47690248c08d6848b4e6427bf2f19a358781111c
Colinr1994 <Colinr1994@gmail.com>
2020-05-17 20:07:42 -0700

    init 4_2

---
+++ hw4_2.cpp
@@ -0,0 +1,24 @@
+/* Title: hw4_2.cpp
+ * Abstract: 
+ *
+ * 
+ *
+ * Author: Colin Reed
+ * ID: 1234
+ * Date: 05/17/2020
+ * HackerRank:
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void input() {} 
+
+int main() {
+	
+
+	return 1;
+}
