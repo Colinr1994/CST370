3a5e990cb4ec12fa81b6512c83c79efa4f774ece
Colinr1994 <Colinr1994@gmail.com>
2020-05-31 19:54:12 -0700

    Planning completed.

--- hw5_3.cpp
+++ hw5_3.cpp
@@ -3,7 +3,7 @@
  *
  * Author: Colin reed
  * ID:1234
- * Date: 5/30/2020
+ * Date: 5/31/2020
  * HackerRank:
  */
 
@@ -11,8 +11,28 @@
 #include <vector>
 using namespace std;
 
-void removeLinks()
+//void removeLinks()
 
+
+/*
+Notes:
+
+vec<vec<int>> storing incoming links. 
+
+if vec is empty, node has no incoming links and can be deleted. 
+
+Links related to deleted node are removed using erase-remove idiom. Search whole vec<vec<>> to find all .
+
+For sample run zero: 
+
+0,1,2,3
+  0,0,0
+      1
+      2
+
+remove node 0 -> search and remove all links origionating form zero. 1,2, and 3 would all have links removed. 
+
+*/
 int main () {
 	int numInd;
 
@@ -23,12 +43,13 @@ int main () {
 	numEdg = 5; 
 	//cin >> numEdg;
 
-	int input[] = {0 1,0 2, 0 3, 1 3, 2 3}
+	int input[] = {0,1,0,2,0,3,1,3,2,3}
 
 	// Graph holds source of incoming links. 
 	vector<vector<int>> graph; 
-
-	for(int i=0;i<numEdg;i++) {
+	graph.resize(numInd);
+	
+	for(int i=0;i<numEdg*2;i++) {
 		
 	}
 
