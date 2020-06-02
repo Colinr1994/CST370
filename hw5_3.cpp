111df31c55247e5d5b4926ac8a5d3faf5280084c
Colinr1994 <Colinr1994@gmail.com>
2020-06-01 19:34:20 -0700

    progress on 5_3. Completed 5_4

--- hw5_3.cpp
+++ hw5_3.cpp
@@ -1,83 +1,83 @@
-/* Title: hw5_2.cpp
- * Abstract: 
+/* Title: hw5_3.cpp
+ * Abstract:
  *
- * Author: Colin reed ID:1234
- * Date: 5/31/2020 HackerRank:
+ * Author: Colin Reed
+ * ID:1234
+ * Date: 5/31/2020
+ * HackerRank:
  */
 
-#include <iostream> 
-#include <vector> 
+#include <iostream>
+#include <vector>
 using namespace std;
-
 
 
 /*
 Notes:
 
-vec<vec<int>> storing incoming links. 
+vec<vec<int>> storing incoming links.
 
-if vec is empty, node has no INCOMING links and can be deleted. 
+if vec is empty, node has no INCOMING links and can be deleted.
 
 Links related to deleted node are removed using erase-remove idiom. Search whole vec<vec<>> to find all .
 
-For sample run zero: 
+For sample run zero:
 
 0,1,2,3
   0,0,0
       1
       2
 
-remove node 0 -> search and remove all links origionating form zero. 1,2, and 3 would all have links removed. 
+remove node 0 -> search and remove all links origionating form zero. 1,2, and 3 would all have links removed.
 */
 
 
 void removeNode(vector<vector<int>> graph, int del) {
-	// First remove component from graph 
-	cout << "Test" << endl;	
+	// First remove component from graph
+	cout << "Test" << endl;
 
-	// Second remove and cout all other instances of that link. 
+	// Second remove and cout all other instances of that link.
 
 }
 
 int main () {
-	
+
 	int numInd;
 	numInd = 4;
 	//cin >> numInd;
-	
+
 	int numEdg;
-	numEdg = 5; 
+	numEdg = 5;
 	//cin >> numEdg;
 
 	int input[] = {0,1,0,2,0,3,1,3,2,3};
 
-	// Graph holds source of incoming links. 
-	vector<vector<int>> graph; 
+	// Graph holds source of incoming links.
+	vector<vector<int>> graph;
 	graph.resize(numInd);
-	
+
 	// Need to revise for final input
 	for(int i=0;i<numEdg*2;i+=2) {
 		int from = input[i];
 		int to = input[i+1];
 		graph[to].push_back(from);
 	}
-	//Graph sucessfully stored. 
-	
+	//Graph sucessfully stored.
+
 	//Display in-degree values
 	for(int i=0;i<numInd;i++) {
-		cout << "In-degree[" << i << "]:" << graph[i].size() << endl;	
+		cout << "In-degree[" << i << "]:" << graph[i].size() << endl;
 	}
-
 
 	// No Longer Safe to use numInd
 
 	cout << "Order:";
 	while(!graph.empty()) {
-		
+
 		// Delete node if subvector is empty
 		for(int i=0;i<numInd;i++) {
 			if(graph[i].empty()) {
-				removeNode(graph, graph[i][0]);
+				//removeNode(graph, graph[i][0]);
 			}
 		}
 
--- hw5_4.cpp
+++ hw5_4.cpp
@@ -1,12 +1,57 @@
-/* Title: hw5_2.cpp
- * Abstract: 
+/* Title: hw5_4.cpp
+ * Abstract:
  *
- * Author: Colin reed ID:1234
- * Date: 5/31/2020 HackerRank:
+ * Author: Colin Reed
+ * ID:1234
+ * Date: 6/1/2020
+ * HackerRank:
  */
 
-#include <iostream> 
-#include <vector> 
+#include <iostream>
+#include <vector>
 using namespace std;
 
+void dfs(int u, int visited[], vector<int> graph[]) {
+	visited[u] = 1;
+	for(int v:graph[u]) {
+		if(visited[v]==0) {
+			dfs(v,visited,graph);
+		}
+	}
+}
 
+int main() {
+  
+	int numVert, numEdge;
+	cin >> numVert;
+   	cin >> numEdge;
+
+   	vector<int> graph[numVert];
+
+   	for(int i=0;i<numEdge;i++) {
+       		int a, b;
+       		cin >> a >> b;
+       		graph[a].push_back(b);
+   	}
+
+   	int visited[numVert];
+   	for(int i=0;i<numVert;i++) {
+		visited[i] = 0;
+	}
+
+   	int prev = -1;
+	bool flag = true;
+   	for(int i=0;i<numVert;i++) {
+       		if(visited[i]==0) {
+           		dfs(i,visited,graph);
+           		if(prev!=-1){
+               			cout << "Edge: " << prev << "-" << i << endl;
+				flag = false;
+			}
+			prev = i;
+		}
+   	}
+	if(flag) {
+		cout << "No new edge:" << endl;
+	}
+}
